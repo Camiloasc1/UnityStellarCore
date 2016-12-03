6c3c950cf7da9cf93f76726d483e567e49b113ab
Camilo Sanchez <caasanchezcu@unal.edu.co>
2016-12-03 18:14:30 -0500

    BackButton Script
    Goes back or closes the app.

---
+++ Assets/Scripts/BackButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace StellarCore
+{
+    public class BackButton : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+                switch (SceneManager.GetActiveScene().name)
+                {
+                    case "MainMenu":
+                        Application.Quit();
+                        break;
+                    case "ProtonProtonChain":
+                        SceneManager.LoadSceneAsync("MainMenu");
+                        break;
+                }
+        }
+    }
+}
