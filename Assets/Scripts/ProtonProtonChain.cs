7936731600acd7d669cfcb234e2e1b49ddd1f9c2
Camilo Sanchez <caasanchezcu@unal.edu.co>
2016-12-04 18:26:47 -0500

    Some equations added

--- Assets/Scripts/ProtonProtonChain.cs
+++ Assets/Scripts/ProtonProtonChain.cs
@@ -17,16 +17,11 @@ namespace StellarCore
         [SerializeField] private RandomButton _random;
 
         private ElementButton[] _buttons;
-        private Elements _currentElement;
 
         public Elements CurrentElement
         {
-            get { return _currentElement; }
-            set
-            {
-                _currentElement = value;
-                _random.Element = _currentElement;
-            }
+            get { return _random.Element; }
+            set { _random.Element = value; }
         }
 
         private void Awake()
@@ -42,6 +37,7 @@ namespace StellarCore
                 var element = button.Element;
                 button.GetComponent<Button>().onClick.AddListener(() => OnElementButtonClick(element));
             }
+//            _random.GetComponent<Button>().onClick.AddListener(()=>OnElementButtonClick(CurrentElement));
             CurrentElement = Elements.Proton;
         }
 
@@ -51,7 +47,7 @@ namespace StellarCore
 
         public void OnElementButtonClick(Elements element)
         {
-            switch (element)
+            switch (CurrentElement)
             {
                 case Elements.Proton:
                     break;
@@ -67,11 +63,88 @@ namespace StellarCore
                     break;
                 case Elements.Li7:
                     break;
+                case Elements.B8:
+                    break;
+                case Elements.Be8:
+                    break;
                 default:
-                    throw new ArgumentOutOfRangeException("element", element, null);
+                    throw new ArgumentOutOfRangeException();
             }
-//            CurrentElement = element;
+            CurrentElement = element;
             //TODO choise random element
+        }
+
+        private void Equation0()
+        {
+//            _proton.Amount -= 2;
+            _H2.Amount++;
+            //TODO charge++
+            //TODO neutrinos++
+            //TODO energy++
+        }
+
+        private void Equation1()
+        {
+//            _proton.Amount--;
+            _H2.Amount--;
+            _He3.Amount++;
+            //TODO energy++
+        }
+
+        private void Equation2()
+        {
+            _He3.Amount -= 2;
+            _He4.Amount++;
+//            _proton.Amount += 2;
+            //TODO energy++
+        }
+
+        private void Equation3()
+        {
+            _He3.Amount--;
+            _He4.Amount--;
+            _Be7.Amount++;
+            //TODO energy++
+        }
+
+        private void Equation4()
+        {
+            _Be7.Amount--;
+            //_electron.Amount--;
+            _Li7.Amount++;
+            //TODO neutrinos++
+            //TODO energy++
+        }
+
+        private void Equation5()
+        {
+            _Li7.Amount--;
+            //_proton.Amount--;
+            _He4.Amount += 2;
+            //TODO energy++
+        }
+
+        private void Equation6()
+        {
+
+        }
+
+        private void Equation7()
+        {
+        }
+
+        private void Equation8()
+        {
+        }
+
+        private void Equation9()
+        {
+            _He3.Amount--;
+//            _proton.Amount--;
+            _He4.Amount ++;
+            //TODO charge++
+            //TODO neutrinos++
+            //TODO energy++
         }
     }
 }
