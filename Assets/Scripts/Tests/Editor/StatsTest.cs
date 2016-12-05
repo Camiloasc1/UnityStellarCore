e78ec577c6fa4ce7fd399cd9f7e94eadad6523e7
Camilo Sanchez <caasanchezcu@unal.edu.co>
2016-12-05 14:56:04 -0500

    Stats tests

--- Assets/Scripts/Tests/Editor/StatsTest.cs
+++ Assets/Scripts/Tests/Editor/StatsTest.cs
@@ -6,15 +6,84 @@ namespace StellarCore.Test
 {
     public class StatsTest
     {
-        [Test]
-        public void ChargeChange()
-        {
-            var gameObject = new GameObject();
-            var stats = gameObject.AddComponent<Stats>();
+        private GameObject _gameObject;
+        private Stats _stats;
 
-            Assert.AreEqual(stats.Energy, 0);
-            Assert.AreEqual(stats.Charge, 0);
-            Assert.AreEqual(stats.Neutrinos, 0);
+        [SetUp]
+        public void SetUp()
+        {
+            _gameObject = new GameObject();
+            _stats = _gameObject.AddComponent<Stats>();
+        }
+
+        [Test]
+        public void ChargeChange1()
+        {
+            Assert.That(_stats.Energy, Is.EqualTo(0).Within(.001));
+            Assert.AreEqual(_stats.Charge, 0);
+
+            _stats.Charge++;
+            Assert.That(_stats.Energy, Is.EqualTo(1.02 * 0).Within(.001));
+            Assert.AreEqual(_stats.Charge, 1);
+
+            _stats.Charge++;
+            Assert.That(_stats.Energy, Is.EqualTo(1.02 * 0).Within(.001));
+            Assert.AreEqual(_stats.Charge, 2);
+
+            _stats.Charge--;
+            Assert.That(_stats.Energy, Is.EqualTo(1.02 * 1).Within(.001));
+            Assert.AreEqual(_stats.Charge, 1);
+
+            _stats.Charge--;
+            Assert.That(_stats.Energy, Is.EqualTo(1.02 * 2).Within(.001));
+            Assert.AreEqual(_stats.Charge, 0);
+
+            _stats.Charge--;
+            Assert.That(_stats.Energy, Is.EqualTo(1.02 * 2).Within(.001));
+            Assert.AreEqual(_stats.Charge, -1);
+
+            _stats.Charge--;
+            Assert.That(_stats.Energy, Is.EqualTo(1.02 * 2).Within(.001));
+            Assert.AreEqual(_stats.Charge, -2);
+
+            _stats.Charge++;
+            Assert.That(_stats.Energy, Is.EqualTo(1.02 * 3).Within(.001));
+            Assert.AreEqual(_stats.Charge, -1);
+
+            _stats.Charge++;
+            Assert.That(_stats.Energy, Is.EqualTo(1.02 * 4).Within(.001));
+            Assert.AreEqual(_stats.Charge, 0);
+        }
+
+        [Test]
+        public void ChargeChange2()
+        {
+            Assert.That(_stats.Energy, Is.EqualTo(0).Within(.001));
+            Assert.AreEqual(_stats.Charge, 0);
+
+            _stats.Charge += 2;
+            Assert.That(_stats.Energy, Is.EqualTo(1.02 * 0).Within(.001));
+            Assert.AreEqual(_stats.Charge, 2);
+
+            _stats.Charge = 0;
+            Assert.That(_stats.Energy, Is.EqualTo(1.02 * 2).Within(.001));
+            Assert.AreEqual(_stats.Charge, 0);
+
+            _stats.Charge = -2;
+            Assert.That(_stats.Energy, Is.EqualTo(1.02 * 2).Within(.001));
+            Assert.AreEqual(_stats.Charge, -2);
+
+            _stats.Charge += 4;
+            Assert.That(_stats.Energy, Is.EqualTo(1.02 * 4).Within(.001));
+            Assert.AreEqual(_stats.Charge, 2);
+
+            _stats.Charge = -2;
+            Assert.That(_stats.Energy, Is.EqualTo(1.02 * 6).Within(.001));
+            Assert.AreEqual(_stats.Charge, -2);
+
+            _stats.Charge = 0;
+            Assert.That(_stats.Energy, Is.EqualTo(1.02 * 8).Within(.001));
+            Assert.AreEqual(_stats.Charge, 0);
         }
     }
 }
