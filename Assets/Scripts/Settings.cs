4785e3aa40310fea831797839d95b394f3ca9c23
Camilo Sanchez <caasanchezcu@unal.edu.co>
2016-12-05 14:15:03 -0500

    Basic Settings Script

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+namespace StellarCore
+{
+    [Serializable]
+    public struct Settings
+    {
+        [SerializeField] private float _massLossRate;
+    }
+}
