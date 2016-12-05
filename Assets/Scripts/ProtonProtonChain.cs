d9eea8412ed4cc28f84beaf9887ae7db32ad3762
Camilo Sanchez <caasanchezcu@unal.edu.co>
2016-12-04 19:52:16 -0500

    Equations assignment and checks

--- Assets/Scripts/ProtonProtonChain.cs
+++ Assets/Scripts/ProtonProtonChain.cs
@@ -33,11 +33,11 @@ namespace StellarCore
         {
             foreach (var button in _buttons)
             {
-                button.Amount = 0;
+                button.Amount = 10;
                 var element = button.Element;
                 button.GetComponent<Button>().onClick.AddListener(() => OnElementButtonClick(element));
             }
-//            _random.GetComponent<Button>().onClick.AddListener(()=>OnElementButtonClick(CurrentElement));
+            _random.GetComponent<Button>().onClick.AddListener(() => OnElementButtonClick(null));
             CurrentElement = Elements.Proton;
         }
 
@@ -45,93 +45,210 @@ namespace StellarCore
         {
         }
 
-        public void OnElementButtonClick(Elements element)
+        public void OnElementButtonClick(Elements? pressed)
         {
-            switch (CurrentElement)
+            if (pressed == null)
+                pressed = CurrentElement;
+            switch (pressed)
             {
                 case Elements.Proton:
+                    switch (CurrentElement)
+                    {
+                        case Elements.Proton:
+                            Equation0();
+                            break;
+                        case Elements.H2:
+                            Equation1();
+                            break;
+                        case Elements.He3:
+                            Equation9();
+                            break;
+                        case Elements.Be7:
+                            Equation6();
+                            break;
+                        default:
+                            Wrong();
+                            break;
+                    }
                     break;
                 case Elements.Electron:
+                    if (CurrentElement == Elements.Be7)
+                        Equation4();
+                    else
+                    {
+                        //charge--;
+                    }
                     break;
                 case Elements.H2:
+                    if (CurrentElement == Elements.Proton)
+                        Equation1();
+                    Wrong();
                     break;
                 case Elements.He3:
+                    switch (CurrentElement)
+                    {
+                        case Elements.Proton:
+                            Equation9();
+                            break;
+                        case Elements.He3:
+                            Equation2();
+                            break;
+                        case Elements.He4:
+                            Equation3();
+                            break;
+                        default:
+                            Wrong();
+                            break;
+                    }
                     break;
                 case Elements.He4:
+                    if (CurrentElement == Elements.He3)
+                        Equation3();
+                    Wrong();
                     break;
                 case Elements.Be7:
+                    switch (CurrentElement)
+                    {
+                        case Elements.Proton:
+                            Equation6();
+                            break;
+                        case Elements.Electron:
+                            Equation4();
+                            break;
+                        default:
+                            Wrong();
+                            break;
+                    }
                     break;
                 case Elements.Li7:
+                    if (CurrentElement == Elements.Proton)
+                        Equation5();
                     break;
                 case Elements.B8:
-                    break;
+                    Equation7();
+                    return;
                 case Elements.Be8:
+                    Equation8();
                     break;
                 default:
-                    throw new ArgumentOutOfRangeException();
+                    Wrong();
+                    break;
             }
-            CurrentElement = element;
             //TODO choise random element
         }
 
+        /// <summary>
+        /// p+ + p+ -> H-2 + e+ + ve
+        /// </summary>
         private void Equation0()
         {
-//            _proton.Amount -= 2;
             _H2.Amount++;
             //TODO charge++
             //TODO neutrinos++
             //TODO energy++
         }
 
+        /// <summary>
+        /// H-2 + p+ -> He-3
+        /// </summary>
         private void Equation1()
         {
-//            _proton.Amount--;
+            if (_H2.Amount < 1)
+            {
+                Wrong();
+                return;
+            }
+
             _H2.Amount--;
             _He3.Amount++;
             //TODO energy++
         }
 
+        /// <summary>
+        /// He-3 + He-3 -> He-4 + p+ + p+
+        /// </summary>
         private void Equation2()
         {
+            if (_He3.Amount < 2)
+            {
+                Wrong();
+                return;
+            }
+
             _He3.Amount -= 2;
             _He4.Amount++;
-//            _proton.Amount += 2;
             //TODO energy++
         }
 
+        /// <summary>
+        /// He-3 + He-4 -> Be-7
+        /// </summary>
         private void Equation3()
         {
+            if (_He3.Amount < 1 || _He4.Amount < 1)
+            {
+                Wrong();
+                return;
+            }
+
             _He3.Amount--;
             _He4.Amount--;
             _Be7.Amount++;
             //TODO energy++
         }
 
+        /// <summary>
+        /// Be-7 + e- -> Li-7 + ve
+        /// </summary>
         private void Equation4()
         {
+            if (_Be7.Amount < 1)
+            {
+                Wrong();
+                return;
+            }
+
             _Be7.Amount--;
-            //_electron.Amount--;
             _Li7.Amount++;
             //TODO neutrinos++
             //TODO energy++
         }
 
+        /// <summary>
+        /// Li-7 + p+ -> He-4 + He-4
+        /// </summary>
         private void Equation5()
         {
+            if (_Li7.Amount < 1)
+            {
+                Wrong();
+                return;
+            }
+
             _Li7.Amount--;
-            //_proton.Amount--;
             _He4.Amount += 2;
             //TODO energy++
         }
 
+        /// <summary>
+        /// Be-7 + p+ -> B-8
+        /// </summary>
         private void Equation6()
         {
+            if (_Be7.Amount < 1)
+            {
+                Wrong();
+                return;
+            }
+
             _Be7.Amount--;
-//            _proton.Amount--;
             _random.Element = Elements.B8;
             //TODO energy++
         }
 
+        /// <summary>
+        /// B-8 -> Be-8 + e+ + ve
+        /// </summary>
         private void Equation7()
         {
             _random.Element = Elements.Be8;
@@ -140,20 +257,36 @@ namespace StellarCore
             //TODO energy++
         }
 
+        /// <summary>
+        /// Be-8 -> He-4 + He-4
+        /// </summary>
         private void Equation8()
         {
-            _He4.Amount++;
+            _He4.Amount += 2;
             //TODO energy++
         }
 
+        /// <summary>
+        /// He-3 + p+ -> He-4 + e+ + ve
+        /// </summary>
         private void Equation9()
         {
+            if (_He3.Amount < 1)
+            {
+                Wrong();
+                return;
+            }
+
             _He3.Amount--;
-//            _proton.Amount--;
             _He4.Amount++;
             //TODO charge++
             //TODO neutrinos++
             //TODO energy++
+        }
+
+        private void Wrong()
+        {
+            Debug.Log("Wrong!");
         }
     }
 }
