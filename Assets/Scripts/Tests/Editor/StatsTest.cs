2eb614fb1698c4cbe6684001bdc3352872f8e129
Camilo Sanchez <caasanchezcu@unal.edu.co>
2016-12-05 14:16:06 -0500

    Playing with unit tests

---
+++ Assets/Scripts/Tests/Editor/StatsTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+namespace StellarCore.Test
+{
+    public class StatsTest
+    {
+        [Test]
+        public void ChargeChange()
+        {
+            var gameObject = new GameObject();
+            var stats = gameObject.AddComponent<Stats>();
+
+            Assert.AreEqual(stats.Energy, 0);
+            Assert.AreEqual(stats.Charge, 0);
+            Assert.AreEqual(stats.Neutrinos, 0);
+        }
+    }
+}
