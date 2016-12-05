01d1f5d6448d608f0a00e64f7b7e8fb58555f6bf
Camilo Sanchez <caasanchezcu@unal.edu.co>
2016-12-05 14:15:40 -0500

    Basic Stats Script

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace StellarCore
+{
+    public class Stats : MonoBehaviour
+    {
+        [SerializeField] private float _energy;
+        [SerializeField] private int _charge;
+        [SerializeField] private uint _neutrinos;
+
+        public float Energy
+        {
+            get { return _energy; }
+            set { _energy = value; }
+        }
+
+        public int Charge
+        {
+            get { return _charge; }
+            set
+            {
+                if (_charge > 0 && value < _charge)
+                    Energy += 1.02f * (_charge - Mathf.Max(value, 0));
+                if (_charge < 0 && value > _charge)
+                    Energy += 1.02f * Mathf.Min(value - _charge, -_charge);
+                _charge = value;
+            }
+        }
+
+        public uint Neutrinos
+        {
+            get { return _neutrinos; }
+            set { _neutrinos = value; }
+        }
+    }
+}
